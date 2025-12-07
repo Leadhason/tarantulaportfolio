2bc512f438e8fe9eac6ddd98fdfea18b437ec6af
Leadhason <leadhason111@gmail.com>
2025-12-07 01:42:41 +0000

    Update portfolio project with new components and assets

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+import Navbar from '#components/Navbar'
+
+function App() {
+
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
